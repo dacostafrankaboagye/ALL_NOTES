29ef65bbea9584dc072fb19fe196db14c587f929
dacostafrankaboagye <frank.aboagye@ashesi.edu.gh>
2024-03-19 07:51:09 +0000

    updated notes

--- Java-learning-and-practicing/TryOut.java
+++ Java-learning-and-practicing/TryOut.java
@@ -3,10 +3,16 @@
 
 public class TryOut {
 
+    String myName;
+    int myAge;
+    boolean myStatus;
 
+    public TryOut(){
+    }
 
     public static void main(String[] args) {
-       
+       TryOut test = new TryOut();
+       System.out.println(test.myName + " " + test.myAge + " " + test.myStatus);
 
 
 
