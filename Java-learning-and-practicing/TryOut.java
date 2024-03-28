72d1b90f82e97913e05c5ce1ff1d715546a6d461
dacostafrankaboagye <frank.aboagye@ashesi.edu.gh>
2024-03-28 20:55:44 +0000

    added generic types

--- Java-learning-and-practicing/TryOut.java
+++ Java-learning-and-practicing/TryOut.java
@@ -1,18 +1,84 @@
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.PrintStream;
-import java.util.Formatter;
-import java.util.Scanner;
+
+class Brand<T> {
+
+    private T name;
+
+    Brand(T name){
+        this.name = name;
+    }
+
+    public T getName() {
+        return this.name;
+    }
+
+    public void setName(T name) {
+        this.name = name;
+    }
+
+}
+
+class Computer {
+
+    private String name;
+    private int serialNumber;
+    private String model;
+
+    Computer(String name, String model, int serialNumber) {
+        this.name = name;
+        this.serialNumber = serialNumber;
+        this.model = model;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getSerialNumber() {
+        return serialNumber;
+    }
+
+    public void setSerialNumber(int serialNumber) {
+        this.serialNumber = serialNumber;
+    }
+
+    public String computerLifeSpan(int years){
+        return " Life Span: " + getModel() + " - " + years;
+    }
+
+}
 
 public class TryOut {
 
     public static void main(String[] args) {
 
-        PrintStream printer = System.out;
+        Computer lenovoComputer = new Computer("Lenovo", "Yoga-7Y", 39800);
+        Computer dellComputer = new Computer("Dell", "SP", 50019);
 
-        InputStream sender = System.in;
+        Brand<Computer> myLenovoBrand = new Brand<Computer>(lenovoComputer);
 
+        System.out.println(myLenovoBrand.getName());
+        
+
+
+
+        
         /*        
+        
+                PrintStream printer = System.out;
+        
+                InputStream sender = System.in;
         try{
 
             int bytedata = sender.read();
@@ -33,19 +99,17 @@ public class TryOut {
                 
                         myScanner.useDelimiter(",");  // anytime it see , -> it recognises that a new pieoce of info is coming
                         myScanner.useDelimiter("\n");  // anytime it see 'a new line' -> it recognises that a new pieoce of info is coming
+                        
+                        Scanner myScanner = new Scanner(System.in);
+                        
+                        System.out.println("Enter here: ");
+                
+                        String data = myScanner.nextLine();
+                
+                        System.out.println(data + "\n");
         */
-        
-        Scanner myScanner = new Scanner(System.in);
-        
-        System.out.println("Enter here: ");
-
-        String data = myScanner.nextLine();
-
-        System.out.println(data + "\n");
 
 
 
     }
 }
-
-
